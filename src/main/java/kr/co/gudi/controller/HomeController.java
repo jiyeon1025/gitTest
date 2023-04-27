ba2736b73f1cacfd4a3d006af273750a92222c60
leejiyeon <dlwldus64533@naver.com>
2023-04-27 09:47:14 +0900

    최초 버전 작성

---
+++ src/main/java/kr/co/gudi/controller/HomeController.java
@@ -0,0 +1,28 @@
+package kr.co.gudi.controller;
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Controller
+public class HomeController {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model) {
+		// 로그 - 찍히는 위치를 알려준다.
+		
+		model.addAttribute("msg", "hello GIT");
+		
+		return "home";
+
+}
+}
